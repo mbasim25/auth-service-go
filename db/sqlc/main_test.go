30c1f0dcceb4f36f46cee558bce3efcc19dc78c3
MohB25 <mohammedbasim2525@gmail.com>
2023-06-21 00:41:36 +0300

    added main test file

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://postgres:password@localhost:5435/void?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("error establishing db connection:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
