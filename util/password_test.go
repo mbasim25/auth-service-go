a45b0cf17bdeb9ce21932dbd264fbece8c7dc865
MohB25 <mohammedbasim2525@gmail.com>
2023-06-26 20:11:22 +0300

    added password test

---
+++ util/password_test.go
@@ -0,0 +1,25 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(6)
+
+	hashedPassword, err := HashPass(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	// Success case
+	err = comparePassword(password, hashedPassword)
+	require.NoError(t, err)
+
+	// Fail case
+	wrongPassword := RandomString(4)
+	err = comparePassword(wrongPassword, hashedPassword)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+}
