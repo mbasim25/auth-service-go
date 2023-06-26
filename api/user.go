33b6b4edde5880e9e47d69b30651a74b8db8a297
MohB25 <mohammedbasim2525@gmail.com>
2023-06-26 20:27:33 +0300

    added validation

--- api/user.go
+++ api/user.go
@@ -5,14 +5,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 
 	db "github.com/mbasim25/void/db/sqlc"
 )
 
 type createUserRequest struct {
-	Username string `json:"username" binding:"required"`
+	Username string `json:"username" binding:"required,alphanum"`
 	Password string `json:"password" binding:"required"`
-	Email    string `json:"email"    binding:"required"`
+	Email    string `json:"email"    binding:"required,email"`
 }
 
 func (server *Server) createUser(ctx *gin.Context) {
@@ -31,6 +32,12 @@ func (server *Server) createUser(ctx *gin.Context) {
 
 	user, err := server.store.CreateUser(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			if pqErr.Code.Name() == "unique_violation" {
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
