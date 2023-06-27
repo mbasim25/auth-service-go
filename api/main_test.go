d8181bc48b56bde24232a742706338a87fff0452
MohB25 <mohammedbasim2525@gmail.com>
2023-06-27 21:33:58 +0300

    added new test server func

--- api/main_test.go
+++ api/main_test.go
@@ -5,10 +5,18 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
+
+	db "github.com/mbasim25/void/db/sqlc"
 )
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
 
 	os.Exit(m.Run())
+}
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	server := NewServer(store)
+
+	return server
 }
